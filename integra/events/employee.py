bd7611abb3d27d51d013d540cb50df8866b48169
FazailFatima <fazailfatima786@gmail.com>
2024-07-10 08:18:04 +0000

    married name issue $ ISS-2024-00021

--- integra/events/employee.py
+++ integra/events/employee.py
@@ -2,7 +2,11 @@
 import frappe
 
 def change_name(doc, method):
+    employee_name_before=doc.first_name + " " + doc.middle_name + "," + doc.last_name + " " + doc.custom_segundo_apellido
     employee_name = doc.first_name + " " + doc.middle_name + "," + doc.last_name + " " + doc.custom_segundo_apellido + " " + "DE" + " " + doc.custom_apellido_de_casada
     if doc.custom_apellido_de_casada:
         doc.db_set("employee_name", employee_name)
+        frappe.db.commit()
+    else:
+        doc.db_set("employee_name", employee_name_before)
         frappe.db.commit()

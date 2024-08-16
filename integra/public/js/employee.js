2fa09930db996ec8b40064c397d47d442e050832
raza9100 <ahmedrazapk>
2024-08-16 10:32:14 +0500

    fixed

--- integra/public/js/employee.js
+++ integra/public/js/employee.js
@@ -5,5 +5,31 @@ frappe.ui.form.on('Employee', {
                 employee: frm.doc.name
             });
         });
+    },
+
+    custom_hik_vision_id: function(frm) {
+        frappe.call({
+            method: 'integra.events.hik_vision.get_data',
+            args: {
+                name: frm.doc.custom_hik_vision_id,
+            },
+            callback: (r) => {
+                if (r.message) {
+                    let data = r.message;
+                    console.log(data);
+                    for (let row of data) {
+                        frm.add_child('custom_records', {
+                            id: row.name,
+                            employee_id: row.employee_id,
+                            access_date_time: row.access_date_time,
+                            access_time: row.access_time,
+                            access_date: row.access_date,
+                        });
+                    }
+
+                    frm.refresh_field('custom_records');
+                }
+            }
+        });
     }
 });

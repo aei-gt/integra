0b8fa9561f8c0b16181a4b96c102dd5e6287e3a9
raza9100 <ahmedrazapk>
2024-08-20 18:41:17 +0500

    employee name issue

--- integra/events/employee.py
+++ integra/events/employee.py
@@ -1,11 +1,19 @@
-
 import frappe
 
 def change_name(doc, method):
-    employee_name = f"""{doc.get('first_name', '')} {doc.get('middle_name', '')}, 
-                        {doc.get('last_name', '')} {doc.get('custom_segundo_apellido', '')}"""
+    first_name = doc.get('first_name', '').strip()
+    middle_name = doc.get('middle_name', '').strip()
+    last_name = doc.get('last_name', '').strip()
+    segundo_apellido = doc.get('custom_segundo_apellido', '').strip()
+    apellido_de_casada = doc.get('custom_apellido_de_casada', '').strip()
 
-    if doc.get('custom_apellido_de_casada'):
-        employee_name += f" DE {doc.get('custom_apellido_de_casada')}"
-    
+    name_parts = [first_name]
+    if middle_name:
+        name_parts.append(middle_name)
+    name_parts.append(last_name)
+    if segundo_apellido:
+        name_parts.append(segundo_apellido)
+    employee_name = ' '.join(name_parts)
+    if apellido_de_casada:
+        employee_name += f" DE {apellido_de_casada}"
     doc.employee_name = employee_name

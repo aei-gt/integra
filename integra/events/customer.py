4f7184987ba9c3edc62a673057909c21472ee24e
its-alikhoker <iamaliraza777@gmail.com>
2025-03-03 20:55:30 +0500

    FIXES

--- integra/events/customer.py
+++ integra/events/customer.py
@@ -3,6 +3,5 @@ from frappe.model.naming import make_autoname
 
 
 def autoname(doc, method):
-    frappe.msgprint("autoname")
     if doc.naming_series:
         doc.name = make_autoname(doc.naming_series)  
